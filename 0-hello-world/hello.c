8bef83603447f1a4da81b3ff275b5e573be4cf9a
Raheel Qamar <rqamar@unc.edu>
2022-01-18 20:35:39 +0000

    a0

---
+++ 0-hello-world/hello.c
@@ -0,0 +1,11 @@
+// PID: 730403179
+// I pledge the COMP 211 honor code.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, world.\n");
+    printf("Welcome to C!\n");
+    return EXIT_SUCCESS;
+}
